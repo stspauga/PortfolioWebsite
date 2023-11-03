4557c6cb0dbdac598135661a2c0f0dc6a6dd9212
tpauga <tpauga@asu.edu>
2023-11-03 14:04:18 -0700

    MatTable

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+const description: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+export interface SkillsColumn {
+  col_one: string;
+  col_two: string;
+  col_three: string;
+}
+
+const skills: SkillsColumn[] = [
+  {col_one: 'Python', col_two: 'Data Structures and Algorithms', col_three: 'Java'},
+  {col_one: 'C/C++', col_two: 'Swift', col_three: 'React'},
+  {col_one: 'Angular', col_two: 'Next.js', col_three: 'Node.js'},
+  {col_one: 'Git', col_two: 'HTML', col_three: 'CSS'},
+  {col_one: 'Javascript', col_two: 'TypeScript', col_three: 'SQL'},
+  {col_one: 'Docker', col_two: 'Agile Methodologies', col_three: 'JavaFX'}
+];
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.css']
 })
 export class AboutMeComponent {
-
+  dataSource = skills;
+  description = description;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -11,8 +12,10 @@ import { AboutMeComponent } from './about-me/about-me.component';
     AboutMeComponent
   ],
   imports: [
+    MatTableModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
