f11f22ec1f2eb59eb3cac8cf764cf6c0cbaf0a7d
tpauga <tpauga@asu.edu>
2023-11-04 15:27:48 -0700

    Completed Jumbotron

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -6,20 +6,23 @@ function scrollFunction() {
   const navLink1 = document.getElementById('nav-link1');
   const navLink2 = document.getElementById('nav-link2');
   const navLink3 = document.getElementById('nav-link3');
+  const navLink4 = document.getElementById('nav-link4');
   if (scrollPosition > 20) {
-    if (navbar != null && navLink1 != null && navLink2 != null && navLink3 != null) {
+    if (navbar != null && navLink1 != null && navLink2 != null && navLink3 != null && navLink4 != null) {
       navbar.classList.remove('navbar');
       navbar.classList.add('navbar-scrolled');
       navLink1.classList.add('nav-link-scrolled');
       navLink2.classList.add('nav-link-scrolled');
       navLink3.classList.add('nav-link-scrolled');
+      navLink4.classList.add('nav-link-scrolled');
     }
   } else if (scrollPosition <= 20) {
-    if (navbar != null && navLink1 != null && navLink2 != null && navLink3 != null) {
+    if (navbar != null && navLink1 != null && navLink2 != null && navLink3 != null && navLink4 != null) {
       navbar.classList.remove('navbar-scrolled');
       navLink1.classList.remove('nav-link-scrolled');
       navLink2.classList.remove('nav-link-scrolled');
       navLink3.classList.remove('nav-link-scrolled');
+      navLink4.classList.remove('nav-link-scrolled');
       navbar.classList.add('navbar');
     }
   }
