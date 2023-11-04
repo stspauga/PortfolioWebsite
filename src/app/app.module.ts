e9a08eb9d0755f6d7d9c41fe7ac37ac0f8bdeebf
tpauga <tpauga@asu.edu>
2023-11-04 01:01:57 -0700

    Icons and scroll functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AboutMeComponent } from './about-me/about-me.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 import { HelloComponent } from './hello/hello.component';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars as faS} from '@fortawesome/free-solid-svg-icons';
+import { faLinkedinIn as LinkedinIn, faGithubSquare as GitHubSq, faFacebookSquare as FBSq, faInstagramSquare as ISq } from '@fortawesome/free-brands-svg-icons';
 
 @NgModule({
   declarations: [
@@ -14,6 +17,7 @@ import { HelloComponent } from './hello/hello.component';
     HelloComponent
   ],
   imports: [
+    FontAwesomeModule,
     MatTableModule,
     BrowserModule,
     AppRoutingModule,
@@ -22,4 +26,8 @@ import { HelloComponent } from './hello/hello.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(library: FaIconLibrary) {
+    library.addIcons(LinkedinIn,GitHubSq,FBSq,ISq,faS)
+  }
+}
--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 
+function scrollFunction() {
+  const scrollPosition = window.scrollY;
+  const navbar = document.getElementById('navbar');
+  if (scrollPosition > 20) {
+    console.log("this part worked");
+    
+  } else if (scrollPosition <= 20) {
+    console.log("we're back");
+  }
+}
 
+window.onscroll = function() {scrollFunction()};
 @Component({
   selector: 'app-hello',
   templateUrl: './hello.component.html',
   styleUrls: ['./hello.component.css']
 })
 export class HelloComponent {
-  
+
 }
