8c7702886a9768bf7cf6cb25f1c7391aa87d6f0f
tpauga <tpauga@asu.edu>
2023-11-06 11:26:23 -0700

    Created the base structure

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+type experiences = {
+  jobTitle: string;
+  companyName: string; 
+  date: string;
+  location: string;
+  detail1?: string;
+  detail2?: string;
+  detail3?: string;
+  detail4?: string;
+}
+
+const experience: experiences[] = [
+  { jobTitle: 'Software Engineer Intern', 
+    companyName: 'US Army Corps of Engineers Engineer Research and Development Center Website (EDRC)',
+    date: 'May 2023 - Current', 
+    location: 'Champaign, Illinois (Remote)', 
+    detail1: 'Proficiently develop React web applications utilizing Next.js, Typescript, hooks and SQL to establish backend RESTful API endpoints and ensure secure database connectivity for data-driven applications',
+    detail2: 'Expertise in Docker for streamlined, consistent deployment across environments'  }
+]
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.css']
 })
-export class ExperienceComponent {}
+export class ExperienceComponent {
+  experience = experience;
+}
 
 
